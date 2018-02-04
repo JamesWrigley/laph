bca7cca08b38f7c0a58f3d4f257ad434d96fa41b
JamesWrigley <james@puiterwijk.org>
2018-02-05 00:13:10 +1300

    Fix Math::y() by first making the argument a global variable

--- src/nodes/Math.jl
+++ src/nodes/Math.jl
@@ -1,3 +1,6 @@
-function y(x, expr)
-    eval(parse(expr))
+function y(args, expr)
+    # eval() works exclusively in the global scope, so we have to elevate our
+    # arguments before calling it.
+    global x = args
+    return eval(parse(expr))
 end
